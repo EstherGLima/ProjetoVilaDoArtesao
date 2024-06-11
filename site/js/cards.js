ca70226e822941c7c9b5aee4d20aad4fbe9b7862
EstherGLima <lima.esther8239@gmail.com>
2024-06-11 19:08:08 -0300

    change all image

--- site/js/cards.js
+++ site/js/cards.js
@@ -1,17 +1,23 @@
-// Dados dos cards
 const cardsData = Array.from({ length: 83 }, (_, i) => i + 1);
 
-// Função para criar os cards
+const imageUrls = Array.from({ length: 83 }, (_, i) => `fotos/finalimage/Chale/${i + 1}.png`);
+
 function createCards() {
   const container = document.querySelector(".card-container");
   container.innerHTML = "";
-  cardsData.forEach((card) => {
+  cardsData.forEach((card, index) => {
     const cardElement = document.createElement("div");
     cardElement.classList.add("card");
-    cardElement.innerText = card;
+
+
+    const imgElement = document.createElement("img");
+    imgElement.src = imageUrls[index % imageUrls.length];
+    cardElement.appendChild(imgElement);
+
     cardElement.addEventListener("click", () => {
-      window.location.href = loja / `${card}`.html;
+      window.location.href = `loja/${card}.html`;
     });
+
     container.appendChild(cardElement);
   });
 }
